alzix/mbed-os

Add ARM_MUSCA_A1 as a new target platform
Musca-A1 is a Cortex-M33 based target with security extension enabled.

- ARM_MUSCA_A1 is the non-secure target running mbed-os.
- ARM_MUSCA_A1_S is the secure target running TF-M.
- TF-M sources were imported and patched in previous commits.
- TF-M secure bootloader (McuBoot) for MUSCA_A1 is submitted by a pre-built binary.
- A post-build hook concatenates The secure and non-secure binaries,
  signs it and then concatenates the bootloader with the signed binary.

targets/TARGET_ARM_SSG/TARGET_MUSCA_A1/device/cmsis.h
@@ -1,0 +1,30 @@
+/*
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MUSCA_CMSIS_H__
+#define __MUSCA_CMSIS_H__
+
+/* CMSIS wrapper for Musca board */
+
+#include "system_cmsdk_musca.h"
+#include "platform_regs.h"              /* Platform registers */
+#include "platform_retarget.h"          /* Peripherals base addresses */
+
+#if defined(COMPONENT_NSPE)
+#include "cmsis_nvic.h"
+#endif
+
+#endif /*__MUSCA_CMSIS_H__ */

nuc972/mbed

targets/TARGET_ARM_SSG/TARGET_MUSCA_A1/device/cmsis.h
@@ -1,0 +1,30 @@
+/*
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MUSCA_CMSIS_H__
+#define __MUSCA_CMSIS_H__
+
+/* CMSIS wrapper for Musca board */
+
+#include "system_cmsdk_musca.h"
+#include "platform_regs.h"              /* Platform registers */
+#include "platform_retarget.h"          /* Peripherals base addresses */
+
+#if defined(TARGET_MUSCA_A1_NS)
+#include "cmsis_nvic.h"
+#endif
+
+#endif /*__MUSCA_CMSIS_H__ */