0ef609922488fd1918407f1f89bc5f9a9173c153
ccli8 <ccli8@nuvoton.com>
2019-04-18 13:59:56 +0800

    Nuvoton fix for PSA

--- TESTS/mbed_hal/spm/main.cpp
+++ TESTS/mbed_hal/spm/main.cpp
@@ -27,8 +27,8 @@
 #error [NOT_SUPPORTED] Disable this Test until FUTURE_SEQUANA_PSA enables Memory protection
 #endif
 
-#if defined(__CORTEX_M33)
-#error [NOT_SUPPORTED] Cannot run on M33 core as SecureFault is implemented in secure-side and cant be remapped
+#if defined(__CORTEX_M23) || defined(__CORTEX_M33)
+#error [NOT_SUPPORTED] Cannot run on M23/M33 core as SecureFault is implemented in secure-side and cant be remapped
 #endif
 
 #include "utest/utest.h"
